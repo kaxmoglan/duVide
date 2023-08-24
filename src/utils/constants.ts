fcddbbad060f8b27443ee8477de43903afcf28d6
Maximilian <42392663+kaxmoglan@users.noreply.github.com>
2023-08-24 17:40:39 +0100

    watch now button

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
 export const EMAIL_ADDRESS = 'dj.du.vide@icloud.com';
 export const PHONE_NUMBER = '+447879903071';
 export const DISPLAY_NUMBER = '+447879 903 071';
+export const YOU_TUBE =
+	'https://www.youtube.com/channel/UCziAUIicwDc3EnhxdyNESKQ';
