fd03f27bbb6ea3d0f5246ab7a8921fa9aba604eb
Maximilian Coghlan <max.coghlan@gmail.com>
2024-03-29 16:58:14 +0000

    Update socials + home button

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,10 @@
 export const EMAIL_ADDRESS = 'dj.du.vide@icloud.com';
 export const PHONE_NUMBER = '+447879903071';
+export const MIX_CLOUD = 'https://www.mixcloud.com/djduvide/';
 export const DISPLAY_NUMBER = '+447879 903 071';
 export const YOU_TUBE =
 	'https://www.youtube.com/channel/UCziAUIicwDc3EnhxdyNESKQ';
+export const INSTAGRAM = 'http://instagram.com/djduvide';
+export const WHATSAPP = 'https://wa.link/wnxrz3';
+export const SPOTIFY =
+	'https://open.spotify.com/user/moleador?si=bb401d06f0f84e28';
